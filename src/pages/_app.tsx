633083dca73bf0368dad71c7d72dc591854b65c2
julia <agramartinsj@gmail.com>
2023-04-14 16:51:28 -0300

    feat: added components to App structure

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,16 @@
-import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import '../styles/global.css'
+import { Header } from '@/components/Header'
+import { Player } from '@/components/Player'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+	return (
+		<div className='flex flex-1'>
+			<main>
+				<Header />
+				<Component {...pageProps} />
+			</main>
+			<Player />
+		</div>
+	)
 }
