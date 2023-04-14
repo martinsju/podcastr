3104e9af9622ca1025ac8b1824a61e4173b9147b
julia <agramartinsj@gmail.com>
2023-04-14 16:50:22 -0300

    feat: created Header component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import format from 'date-fns/format'
+import ptBR from 'date-fns/locale/pt-BR'
+
+export const Header: React.FC = () => {
+	const currentDate = format(new Date(), 'EEEEEE, d MMMM', {
+		locale: ptBR
+	})
+	return (
+		<header className='px-8 py-4 bg-white flex items-center'>
+			<h3 className='text-gray-800'>Podcastr</h3>
+			<p className='text-gray-400 text-xs ml-2 px-1 border-l border-l-gray-100'>
+				O melhor pra você ouvir, sempre
+			</p>
+			<span className='text-gray-400 text-xs capitalize ml-auto'>
+				{currentDate}
+			</span>
+		</header>
+	)
+}
