ea9e20b56f85cff086331ee02c5f56ee6d7980c2
julia <agramartinsj@gmail.com>
2023-04-18 16:04:03 -0300

    feat: static call api response w/ getstaticprops

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,33 @@
-import { Inter } from 'next/font/google'
+import { GetStaticProps } from 'next'
 
-const inter = Inter({ subsets: ['latin'] })
+type Episode = {
+	id: string
+	title: string
+	members: string
+}
 
-export default function Home() {
-	return <div className='container'></div>
+type HomeProps = {
+	episodes: Array<Episode>
+}
+
+export default function Home(props: HomeProps) {
+	return (
+		<div className='container'>
+			<p>{JSON.stringify(props)}</p>
+		</div>
+	)
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+	const response = await fetch(
+		'http://localhost:3333/episodes?_limit=12&_sort=published_at&_order=desc'
+	)
+	const data = await response.json()
+
+	return {
+		props: {
+			episodes: data
+		},
+		revalidate: 60 * 60 * 8 //a cada 8h
+	}
 }
