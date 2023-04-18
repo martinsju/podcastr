e2efc98860d37407a98c0319ecb27f37e0c41e16
julia <agramartinsj@gmail.com>
2023-04-18 13:03:11 -0300

    build/styles: new preset for gradient angle

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
 	content: [
 		'./src/pages/**/*.{js,ts,jsx,tsx}',
@@ -10,7 +11,8 @@ module.exports = {
 			backgroundImage: {
 				'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
 				'gradient-conic':
-					'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))'
+					'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+				'gradient-143.8': 'linear-gradient(143.8deg, var(--tw-gradient-stops))'
 			},
 			fontFamily: {
 				sans: ['Inter', 'Lexend']
