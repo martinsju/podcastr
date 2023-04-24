46e4824ccfac1655246a69c5aef195b307aa8d7b
julia <agramartinsj@gmail.com>
2023-04-24 20:54:16 -0300

    feat: data formatting get by axios

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,32 +1,77 @@
 import { GetStaticProps } from 'next'
+import { format, parseISO } from 'date-fns'
+import ptBR from 'date-fns/locale/pt-BR'
+import { api } from '@/components/services/api'
+import { convertDurationToTimeString } from '@/utils/convertDurationToTimeString'
 
 type Episode = {
 	id: string
 	title: string
+	thumbnail: string
 	members: string
+	publishedAt: string
+	url: string
+	durationAsString: string
+	description: string
 }
 
 type HomeProps = {
 	episodes: Array<Episode>
 }
 
+type EpisodeData = {
+	id: string
+	title: string
+	thumbnail: string
+	members: string
+	published_at: string
+	file: {
+		duration: number
+		url: string
+	}
+	description: string
+}
+
 export default function Home(props: HomeProps) {
 	return (
 		<div className='container'>
-			<p>{JSON.stringify(props)}</p>
+			<h1>Index</h1>
+			<p>Podcast: {JSON.stringify(props.episodes[0].title)}</p>
 		</div>
 	)
 }
 
 export const getStaticProps: GetStaticProps = async () => {
-	const response = await fetch(
-		'http://localhost:3333/episodes?_limit=12&_sort=published_at&_order=desc'
-	)
-	const data = await response.json()
+	const { data } = await api.get('episodes', {
+		params: {
+			_limit: 12,
+			_sort: 'published_at',
+			_order: 'desc'
+		}
+	})
+
+	//format data before rendering it
+	const episodes = data.map((episode: EpisodeData) => {
+		return {
+			id: episode.id,
+			title: episode.title,
+			thumbnail: episode.thumbnail,
+			members: episode.members,
+			publishedAt: format(parseISO(episode.published_at), 'd MMM yy', {
+				locale: ptBR
+			}),
+			duration: Number(episode.file.duration),
+			durationAsString: convertDurationToTimeString(
+				Number(episode.file.duration)
+			),
+			description: episode.description,
+			url: episode.file.url
+		}
+	})
 
 	return {
 		props: {
-			episodes: data
+			episodes
 		},
 		revalidate: 60 * 60 * 8 //a cada 8h
 	}
