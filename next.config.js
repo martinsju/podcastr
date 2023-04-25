8eb4fd081ba4a9e6356dcf9794ec889b7dcd3ea4
julia <agramartinsj@gmail.com>
2023-04-24 22:35:21 -0300

    feat: added images path @next.config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+	reactStrictMode: true,
+	images: {
+		remotePatterns: [
+			{
+				protocol: 'https',
+				hostname: 'storage.googleapis.com',
+				port: '',
+				pathname: '/**'
+			}
+		]
+	}
 }
+//https://storage.googleapis.com/golden-wind/nextlevelweek/05-podcastr/opensource.jpg
 
 module.exports = nextConfig
