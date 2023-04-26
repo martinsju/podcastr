136c84f66c4b8d3c7eaf00a5d3317f9cb3a2a55c
julia <agramartinsj@gmail.com>
2023-04-26 17:39:58 -0300

    style: styled lastEpisodes elements

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -36,33 +36,56 @@ type EpisodeData = {
 
 export default function Home({ latestEpisodes, allEpisodes }: HomeProps) {
 	return (
-		<div id='homePage' className=''>
+		<div
+			id='homePage'
+			className='py-0 px-16 h-[calc(100vh-6.5rem)] overflow-y-scroll '
+		>
 			<section id='lastEpisodes'>
-				<h2>Últimos lançamentos</h2>
-				<ul>
+				<h2 className='mt-12 mb-6'>Últimos lançamentos</h2>
+				<ul className='list-none grid grid-cols-2 gap-6'>
 					{latestEpisodes.map((episode) => {
 						return (
-							<li key={episode.id}>
+							<li
+								key={episode.id}
+								className='bg-white border border-gray-100 p-5 rounded-3xl relative flex items-center'
+							>
 								<Image
 									width={192}
 									height={192}
 									src={episode.thumbnail}
 									alt={episode.title}
 									style={{ objectFit: 'cover' }}
+									className='w-24 h-24 rounded-2xl'
 								/>
-								<div id='episodeDetails'>
-									<a href=''>{episode.title}</a>
-									<p>{episode.members}</p>
-									<span>{episode.publishedAt}</span>
-									<span>{episode.durationAsString}</span>
+
+								<div id='episodeDetails' className='flex-1 ml-4'>
+									<a
+										href=''
+										className='block text-gray-800 leading-[1.4rem] font-semibold no-underline hover:underline'
+									>
+										{episode.title}
+									</a>
+									<p className='text-sm mt-2 max-w-[70%] whitespace-nowrap overflow-hidden text-ellipsis'>
+										{episode.members}
+									</p>
+									<span className='inline-block mt-2 text-sm'>
+										{episode.publishedAt}
+									</span>
+									<span className='inline-block mt-2 text-sm ml-2 pl-2 relative before:content-[""] before:w-1 before:h-1 before:rounded-sm before:bg-[#DDD] before:absolute before:left-0 before:top-1/2 before:translate-x-[-50%] before:translate-y-[-50%]'>
+										{episode.durationAsString}
+									</span>
 								</div>
 
-								<button type='button'>
+								<button
+									type='button'
+									className='absolute right-8 bottom-8 w-10 h-10 bg-white border border-gray-100 rounded-[0.675rem] text-[0] transition duration-200 hover:brightness-95'
+								>
 									<Image
-										width={10}
-										height={10}
+										width={120}
+										height={120}
 										src='/play-green.svg'
 										alt='Tocar episódio'
+										className='w-6 h-6 m-auto'
 									/>
 								</button>
 							</li>
@@ -104,8 +127,8 @@ export const getStaticProps: GetStaticProps = async () => {
 		}
 	})
 
-	const latestEpisodes: Episode = episodes.slice(0, 2)
-	const allEpisodes: Episode = episodes.slice(2, episodes.length)
+	const latestEpisodes = episodes.slice(0, 2)
+	const allEpisodes = episodes.slice(2, episodes.length)
 
 	return {
 		props: {
