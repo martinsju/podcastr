2acd91d971e1a17ea20dcc866365f9b011974fd3
julia <agramartinsj@gmail.com>
2023-04-27 20:16:35 -0300

    style: styled episode page

--- src/pages/episodes/[slug].tsx
+++ src/pages/episodes/[slug].tsx
@@ -31,11 +31,20 @@ export default function Episode({ episode }: EpisodeProps) {
 	return (
 		<div
 			id='episode'
-			className='py-0 px-16 h-[calc(100vh-6.5rem)] overflow-y-scroll'
+			className='py-12 px-8 max-w-[45rem] h-[calc(100vh-6.5rem)] my-0 mx-auto '
 		>
-			<div id='thumbnailContainer'>
-				<button type='button'>
-					<Image width={192} height={192} src='/arrow-left.svg' alt='Voltar' />
+			<div id='thumbnailContainer' className='relative'>
+				<button
+					type='button'
+					className='w-12 h-12 p-3 text-[0] rounded-xl border-0 absolute z-[5] transition duration-200 hover:brightness-95 left-0 top-1/2 bg-blue-500 -translate-x-1/2 -translate-y-1/2'
+				>
+					<Image
+						width={192}
+						height={192}
+						src='/arrow-left.svg'
+						alt='Voltar'
+						className='rounded-2xl'
+					/>
 				</button>
 				<Image
 					width={700}
@@ -43,26 +52,38 @@ export default function Episode({ episode }: EpisodeProps) {
 					src={episode.thumbnail}
 					alt={episode.title}
 					style={{ objectFit: 'cover' }}
+					className='rounded-2xl w-[700px] h-[160px]'
 				/>
-				<button type='button'>
+				<button
+					type='button'
+					className='w-12 h-12 p-3 text-[0] rounded-xl border-0 absolute z-[5] transition duration-200 hover:brightness-95 right-0 top-1/2 bg-green-500 translate-x-1/2 -translate-y-1/2'
+				>
 					<Image
 						width={192}
 						height={192}
 						src='/play.svg'
 						alt='Tocar episódio'
+						className='rounded-2xl'
 					/>
 				</button>
 			</div>
 
-			<header>
-				<h1>{episode.title}</h1>
-				<span>{episode.members}</span>
-				<span>{episode.publishedAt}</span>
-				<span>{episode.durationAsString}</span>
+			<header className='pb-4 border-b border-b-gray-200'>
+				<h1 className='mt-8 mb-6 text-gray-800 leading-10 font-semibold font-secondary no-underline hover:underline'>
+					{episode.title}
+				</h1>
+				<span className='inline-block text-sm '>{episode.members}</span>
+				<span className='inline-block text-sm ml-4 pl-4 relative before:content-[""] before:w-1 before:h-1 before:rounded-sm before:bg-[#DDD] before:absolute before:left-0 before:top-1/2 before:translate-x-[-50%] before:translate-y-[-50%]'>
+					{episode.publishedAt}
+				</span>
+				<span className='inline-block text-sm ml-4 pl-4 relative before:content-[""] before:w-1 before:h-1 before:rounded-sm before:bg-[#DDD] before:absolute before:left-0 before:top-1/2 before:translate-x-[-50%] before:translate-y-[-50%]'>
+					{episode.durationAsString}
+				</span>
 			</header>
 
 			<div
 				id='description'
+				className='mt-8 leading-[1.675rem] text-gray-700 [&>p]:my-6 [&>p]:mx-0'
 				dangerouslySetInnerHTML={{ __html: episode.description }}
 			/>
 		</div>
