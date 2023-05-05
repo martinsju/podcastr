33db0ad14cf3ebafc8544fb03cd1f50e716868f6
julia <agramartinsj@gmail.com>
2023-05-05 20:26:05 -0300

    feat: added functions for handle audio list navigation

--- src/contexts/PlayerContext.tsx
+++ src/contexts/PlayerContext.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { ReactNode, useState } from 'react'
 import { createContext } from 'react'
 
 type EpisodeContext = {
@@ -15,7 +15,76 @@ type PlayerContextData = {
 	//currentEpisode is an array position
 	isPlaying: boolean
 	play: (episode: EpisodeContext) => void
+	playList: (list: Array<EpisodeContext>, index: number) => void
 	togglePlay: () => void
+	playNext: () => void
+	playPrevious: () => void
+	hasNext: boolean
+	hasPrevious: boolean
 }
 
 export const PlayerContext = createContext({} as PlayerContextData)
+
+type PlayerContextProviderProps = {
+	children: ReactNode
+}
+
+export const PlayerContextProvider = ({
+	children
+}: PlayerContextProviderProps) => {
+	const [episodeList, setEpisodeList] = useState<Array<EpisodeContext>>([])
+	const [currentEpisodeIndex, setCurrentEpisodeIndex] = useState(0)
+	const [isPlaying, setIsPlaying] = useState(true)
+
+	const hasNext = currentEpisodeIndex + 1 < episodeList.length
+	const hasPrevious = currentEpisodeIndex > 0
+
+	function play(episode: EpisodeContext) {
+		setEpisodeList([episode])
+		setCurrentEpisodeIndex(0)
+		setIsPlaying(true)
+		// console.log('clicou no episodio ', episode.title)
+	}
+
+	function playList(list: Array<EpisodeContext>, index: number) {
+		setEpisodeList(list)
+		setCurrentEpisodeIndex(index)
+		setIsPlaying(true)
+		// console.log('playlist episodio ', list[index])
+	}
+
+	function togglePlay() {
+		setIsPlaying((isPlaying) => !isPlaying)
+	}
+
+	function playNext() {
+		if (hasNext) {
+			setCurrentEpisodeIndex(currentEpisodeIndex + 1)
+		}
+	}
+
+	function playPrevious() {
+		if (hasPrevious) {
+			setCurrentEpisodeIndex(currentEpisodeIndex - 1)
+		}
+	}
+
+	return (
+		<PlayerContext.Provider
+			value={{
+				episodeList,
+				currentEpisodeIndex,
+				play,
+				playList,
+				isPlaying,
+				togglePlay,
+				playNext,
+				playPrevious,
+				hasNext,
+				hasPrevious
+			}}
+		>
+			{children}
+		</PlayerContext.Provider>
+	)
+}
