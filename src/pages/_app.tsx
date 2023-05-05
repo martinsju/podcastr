381731470e3e3fc50d8b383c12a046aaca04ad35
julia <agramartinsj@gmail.com>
2023-05-05 20:25:00 -0300

    refactor: moved functions and states to playerContext

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,8 @@
 import type { AppProps } from 'next/app'
-import '../styles/global.css'
 import { Header } from '@/components/Header'
 import { Player } from '@/components/Player'
-import { PlayerContext } from '@/contexts/PlayerContext'
-import { useState } from 'react'
+import '../styles/global.css'
+import { PlayerContextProvider } from '@/contexts/PlayerContext'
 
 type EpisodeApp = {
 	title: string
@@ -14,25 +13,8 @@ type EpisodeApp = {
 }
 
 export default function App({ Component, pageProps }: AppProps) {
-	const [episodeList, setEpisodeList] = useState<Array<EpisodeApp>>([])
-	const [currentEpisodeIndex, setCurrentEpisodeIndex] = useState(0)
-	const [isPlaying, setIsPlaying] = useState(true)
-
-	function play(episode: EpisodeApp) {
-		setEpisodeList([episode])
-		setCurrentEpisodeIndex(0)
-		setIsPlaying(true)
-		console.log('clicou no episodio ', episode.title)
-	}
-
-	function togglePlay() {
-		setIsPlaying((isPlaying) => !isPlaying)
-	}
-
 	return (
-		<PlayerContext.Provider
-			value={{ episodeList, currentEpisodeIndex, play, isPlaying, togglePlay }}
-		>
+		<PlayerContextProvider>
 			<div className='flex'>
 				<main className='flex-1'>
 					<Header />
@@ -40,6 +22,6 @@ export default function App({ Component, pageProps }: AppProps) {
 				</main>
 				<Player />
 			</div>
-		</PlayerContext.Provider>
+		</PlayerContextProvider>
 	)
 }
